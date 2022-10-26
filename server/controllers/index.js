0c331ad5ad0588d1dbb055e1f57028eaf4cc9974
Arpan <gohel.arpan@gmail.com>
2022-10-26 16:11:56 -0600

    updated performLogout

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -130,6 +130,8 @@ module.exports.processRegisterPage = (req, res, next) => {
 }
 
 module.exports.performLogout = (req, res, next) => {
-    req.logout();
+    req.logout(function(err){
+      if(err){return next(err);}
     res.redirect('/');
+    });
 }
