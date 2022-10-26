af0428a51d43f9d779f322707e5db4df7c192c95
Arpan <gohel.arpan@gmail.com>
2022-10-26 09:26:13 -0600

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+    //"URI":"mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.0/business_contacts"
+    "URI":"mongodb://127.0.0.1:27017/business_contacts"
+}
---
+++ server/routes/contact.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+let passport = require('passport');
+
+// connect to our Contacts Model
+let Contact = require('../models/contact');
+
+let contactController = require('../controllers/contact');
+
+//helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    //check if the user is logged in 
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+//GET Route for the contact list page - READ operation
+router.get('/', requireAuth, contactController.displayContactList);
+
+//GET Route for displaying Add page - CREATE Operation
+router.get('/add', requireAuth, contactController.displayAddPage);
+
+//POST Route for processing the Add page - CREATE Operation
+router.post('/add', requireAuth, contactController.processAddPage);
+
+//GET Route for displaying the Edit page - UPDATE Operation
+router.get('/edit/:id', requireAuth, contactController.displayEditPage);
+
+//POST Route for processing the Edit page - UPDATE Operation
+router.post('/edit/:id', requireAuth, contactController.processEditPage);
+
+//GET Route to perform Deletion - DELETE Operation 
+router.get('/delete/:id', requireAuth, contactController.performDelete);
+
+
+module.exports = router;
