f10ab204d2e6e7e4931ca0b7178f0755527e01f7
Arpan Gohel <gohel.arpan@gmail.com>
2024-06-05 21:59:58 -0600

    new URI key

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@
 
 module.exports =
 {
-    //"URI":"mongodb://127.0.0.1:27017/business_contacts"
-    "URI":"mongodb+srv://Arpan:9Sep1990@mongodbserver.whzyks1.mongodb.net/?retryWrites=true&w=majority"
-    //"Secret": 'SomeSecret'
+    "URI":"mongodb://127.0.0.1:27017/business_contacts",
+    "URI":"mongodb+srv://gohelarpan:9Sep1990@portfolio-db.abte2do.mongodb.net/",
+    "Secret": 'SomeSecret'
 }
