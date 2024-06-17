2b2c077a77989779bfce7ce1b30344b648ee6c2f
Arpan Gohel <gohel.arpan@gmail.com>
2024-06-16 23:07:18 -0600

    URI changed

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,6 @@
 module.exports =
 {
     //"URI":"mongodb://127.0.0.1:27017/business_contacts",
-    "URI":"mongodb+srv://gohelarpan:9Sep1990@portfolio-db.abte2do.mongodb.net/",
+    "URI":"mongodb+srv://gohelarpan:9Sep1990@portfolio-db.abte2do.mongodb.net/?retryWrites=true&w=majority&appName=Portfolio-DB",
     "Secret": 'SomeSecret'
 }
